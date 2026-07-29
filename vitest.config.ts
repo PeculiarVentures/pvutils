50426d3bd7d8b36dc11cfa329c30e75c140a9b69
Matteo Bonzanini <mat.bonza@gmail.com>
2026-07-29 13:18:49 +0300

    chore(lint): complete configuration after migration

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["test/**/*.ts"],
-    testTimeout: 10000,
     coverage: {
       provider: "v8",
       reporter: ["lcov", "text-summary"],
